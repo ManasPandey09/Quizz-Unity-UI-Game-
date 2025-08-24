fa23b60b2537fd53ab9806eb4aceba45aaff46d5
ManasPandey09 <manaspandey2709@gmail.com>
2025-08-24 16:32:25 +0530

    questions

--- Assets/Scripts/QuestionSO.cs
+++ Assets/Scripts/QuestionSO.cs
@@ -1,7 +1,30 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "QuestionSO", menuName = "Scriptable Objects/QuestionSO")]
+[CreateAssetMenu(fileName = "Question", menuName = "New Question")]
 public class QuestionSO : ScriptableObject
 {
-    
+    [TextArea(2, 6)]
+    [SerializeField] string question = "Enter question text here";
+    [SerializeField] string[] answers = new string[4];
+    [SerializeField] int correctAnswerIndex;
+
+    public string GetQuestion()
+    {
+        return question;
+    }
+
+    public int GetCorrectAnswerIndex()
+    {
+        return correctAnswerIndex;
+    }
+
+    public string GetAnswer(int index)
+    {
+        if (index < 0 || index >= answers.Length)
+        {
+            Debug.LogError("Index out of range");
+            return null;
+        }
+        return answers[index];
+    }
 }
